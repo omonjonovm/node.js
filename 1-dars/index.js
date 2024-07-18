3d2656fd68b834c0c9aee3c984130955bdbfced5
omonjonovm <omonjonovmurodjon2@gmail.com>
2024-07-18 15:09:03 +0500

    json

--- 1-dars/index.js
+++ 1-dars/index.js
@@ -18,24 +18,21 @@ const http = require('http')
 const path = require('path')
 const fs = require('fs')
 
-const server = http.createServer((req,res) => {
-  if(req.url === '/'){
-    fs.readFile(path.join(__dirname,'public','index.html'), (err,content) => {
-      if(err) throw err
-      res.writeHead(200,{'Content-Type':'text/html'})
-      res.end(content)
-    })
-  }  else if(req.url ==='/about'){
-    fs.readFile(path.join(__dirname,'public','about.html'), (err,content) => {
-      if(err) throw err
-      res.writeHead(200,{'Content-Type':'text/html'})
-      res.end(content)
-    })
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    const books = [
+      { name: 'Atomic-Hobits', author: 'James Clir', pages: 250 },
+      { name: 'Men', author: 'Boltayev Tesha', pages: 25 },
+      { name: 'Molxona', author: 'Sharofat Ergasheva', pages: 100 },
+    ]
+
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify(books))
   }
 })
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT,() => {
+server.listen(PORT, () => {
   console.log(`Server ${PORT} portda ishga tushdi`);
 })
